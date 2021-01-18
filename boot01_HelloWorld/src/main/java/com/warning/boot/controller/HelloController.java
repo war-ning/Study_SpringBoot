0042670975847a2931db90bcfd7e97b35164d943
War_ning <war_ning@yeah.net>
2021-01-18 11:59:25 +0800

    Hello SpringBoot

---
+++ boot01_HelloWorld/src/main/java/com/warning/boot/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.warning.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String helloWorld(){
+        return "Hello,Spring Boot 2!";
+    }
+}
